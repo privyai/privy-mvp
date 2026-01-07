7b3ea9fb4334ee80acc05d7bb13d5b5370da1dc9
Paritosh Kulkarni <pari2798@gmail.com>
2026-01-06 18:49:16 -0800

    feat: Add z-index properties to interactive hover button elements for correct layering.

--- components/ui/interactive-hover-button.tsx
+++ components/ui/interactive-hover-button.tsx
@@ -20,14 +20,14 @@ const InteractiveHoverButton = React.forwardRef<
             )}
             {...props}
         >
-            <span className="inline-block translate-x-1 transition-all duration-300 group-hover:translate-x-12 group-hover:opacity-0">
+            <span className="relative z-10 inline-block translate-x-1 transition-all duration-300 group-hover:translate-x-12 group-hover:opacity-0">
                 {text}
             </span>
-            <div className="absolute left-0 top-0 flex h-full w-full items-center justify-center gap-2 text-primary-foreground opacity-0 transition-all duration-300 group-hover:opacity-100">
+            <div className="absolute left-0 top-0 z-10 flex h-full w-full items-center justify-center gap-2 text-primary-foreground opacity-0 transition-all duration-300 group-hover:opacity-100">
                 <span>{text}</span>
                 <ArrowRight className="h-4 w-4" />
             </div>
-            <div className="absolute left-[5%] top-[40%] h-2 w-2 scale-[1] rounded-lg bg-primary transition-all duration-300 group-hover:left-[0%] group-hover:top-[0%] group-hover:h-full group-hover:w-full group-hover:scale-[1.8] group-hover:bg-primary z-0"></div>
+            <div className="absolute left-[5%] top-[40%] z-0 h-2 w-2 scale-[1] rounded-lg bg-primary transition-all duration-300 group-hover:left-[0%] group-hover:top-[0%] group-hover:h-full group-hover:w-full group-hover:scale-[1.8] group-hover:bg-primary"></div>
         </button>
     );
 });
